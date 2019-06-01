1dee476163fd3e4bb1c6ccccda4241f921b830e1
David Sturm <david.sturm@tu-dortmund.de>
2019-06-01 20:27:29 +0200

    Added Appegios to PunkChords

--- Generators/PunkChords.py
+++ Generators/PunkChords.py
@@ -3,56 +3,41 @@ from music21 import *
 from Generators.Chords_Util import *
 #import Generators.Chords_Util
 
-class PopChords:
+class PunkChords:
 
     def __init__(self):
         pass
         self.chords_music = stream.Part()
 
-    def generate(self, mykey, mycompl, mytempo, myscale, mygenre, singlechordlength=0.25):
+    def generate(self, mykey = "C", mycompl = 3, mytempo = 4, myscale = "Major", mygenre = "Punk", singlechordlength=0.25, length = 4):
 
         self.chords_music = stream.Part()
         self.chords_music.append(tempo.MetronomeMark(number=mytempo))
-
+        self.chords_music.append(instrument.ElectricGuitar())
         pattern = self.generatePattern(length=4)
-        #print(mykey + " " + myscale)
         mykeyobj = key.Key(mykey, myscale)
+        self.genAppegioChords(mykeyobj, self.chords_music, pattern, 0.125,
+                            self.generateEightsBeat(singlechordlength=0.125, chordlength=1, mygenre=mygenre,
+                                                    mycompl=mycompl),
+                            [0,2,4])
 
-        if mycompl == 1 or mycompl == 2:
-            pattern = pattern*2
-            pattern += pattern
-        elif mycompl == 3 or mycompl == 4:
-            pattern = pattern + self.generatePattern()
-            pattern += pattern
-        elif mycompl == 5:
-            pattern = pattern + self.generatePattern() + self.generatePattern() + self.generatePattern()
-        #print("the pattern is: {} with length {} ".format(pattern, len(pattern)))
-        if "punk" in mygenre or 40 <= mytempo:
-            Chords_Util.genChords(mykeyobj, self.chords_music, pattern, 0.25, self.generateBeat(0.125, 1, mygenre, mycompl), [0, 4])
-            #self.chords_music.append(Chords_Util.genChords(mykeyobj, pattern, 0.125,  self.generateBeat(0.125, 1, mygenre, mycompl),[0,2]))
-        else:
-            #self.chords_music.append(Chords_Util.genChords(mykeyobj, pattern, 0.25, self.generateBeat(0.25, 1, mygenre,mycompl),[0,2,4]))
-            Chords_Util.genChords(mykeyobj, self.chords_music, pattern, 0.25, self.generateBeat(0.25, 1, mygenre,mycompl),[0,2,4])
-
+        self.genMutedChords(mykeyobj, self.chords_music, pattern, 0.125,
+                       self.generateEightsBeat(singlechordlength=0.125, chordlength=1, mygenre=mygenre, mycompl=mycompl),
+                       [0, 4])
         return self.chords_music.__deepcopy__()
 
-    def generateBeat(self, singlechordlength, chordlength, mystyle, mycompl):
-        if not(chordlength/singlechordlength).is_integer():
-            print("Could not create beat, please have chordlength be a multilple of singlechordlength")
-            raise NotImplementedError
-        return self.generateGenericBeat(singlechordlength, chordlength, mystyle, mycompl)
-
-
-    def generateGenericBeat(self, singlechordlength, chordlength, mystyle, mycompl):
-        #print("generating generic beat")
+    def generateEightsBeat(self, singlechordlength=0.125, chordlength = 1, mygenre = "Punk", mycompl = 3):
         sum = 0
         beat = []
         maxlength = chordlength/singlechordlength
         while sum < maxlength:
             while(True):
-                x = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 8]
-                #x = [0,1]
-                #x = [0,0,0,1]
+                if(sum == (maxlength-1)):
+                    x = [1]
+                elif(sum < maxlength/2):
+                    x = [0, 1, 1, 1, 2, 2]
+                else:
+                    x = [0, 1, 1, 1, 1, 1, 1, 2]
                 currentbeat = rn.choice(x)
                 currentbeatvalue = 0
                 if currentbeat == 0:
@@ -62,31 +47,50 @@ class PopChords:
                 #print("the current sum is {}, the currentbeatvalue is {}, the current maxlength is{}".format(sum, currentbeatvalue, maxlength))
                 if sum+currentbeatvalue <= maxlength:
                     beat += [int(currentbeat)]
-                    sum  += currentbeatvalue
+                    sum += currentbeatvalue
                     break
-        #print("the beat is {}".format(beat))
         return beat
 
-    '''
-    def genChords(self, key, pattern, singlechordlength, beat):
-        print(pattern)
+    def genMutedChords(self, key, chords_music, pattern, singlechordlength, beat, notesInChords):
         for basenote in pattern:
             for singlebeat in beat:
                 if singlebeat == 0:
                     chord_music = note.Rest(duration=duration.Duration(singlechordlength))
-                elif singlechordlength <= 0.125:
-                    chord_music = self.getChord(key, basenote, singlechordlength*singlebeat, [0, 4])
                 else:
-                    chord_music = self.getChord(key, basenote, singlechordlength*singlebeat, [0, 2, 4])
-                self.chords_music.append(chord_music.__deepcopy__())
+                    chord_music = self.getChord(key, basenote, singlechordlength*singlebeat, notesInChords)
+                    if(singlebeat > 1):
+                        chord_music.volume = volume.Volume(velocity=80)
+                    else:
+                        chord_music.volume = volume.Volume(velocity=50)
+                chords_music.append(chord_music)
+
+
+    def genAppegioChords(self, mykey, chords_music, pattern, singlenotelength, beat, notesInChords):
+        muster = []
+        for _ in (range(int(1 / singlenotelength))):
+            muster += [rn.choice(notesInChords)]
+        print(muster)
+        for basenote in pattern:
+            #mychord = self.getChord(key, basenote, singlenotelength, notesInChords)
+            for singlenote in muster:
+                mynotename = mykey.pitches[(singlenote+basenote)%7]
+                if (singlenote+basenote>6):
+                    mynotename.octave = mynotename.implicitOctave +1
+                mynotename.octave += rn.choice([0, 0, 0, 0])
+                mynote = note.Note(mynotename, duration=duration.Duration(singlenotelength))
+                chords_music.append(mynote)
+        return chords_music
 
     def getChord(self, key, basenote, singlechordlength, chordnotes):
-        chord_music = stream.Part()
         mychord = chord.Chord(duration=duration.Duration(singlechordlength))
         mypitches = key.pitches
         for i in chordnotes:
-               mychord.add(mypitches[(basenote+i)%len(mypitches)])
-        return mychord'''
+               #mychord.add(mypitches[(basenote+i)%len(mypitches)])
+               mypitch = mypitches[(basenote + i) % 7]
+               if basenote+i > 6:
+                   mypitch.octave += 1
+               mychord.add(mypitch)
+        return mychord
 
 
     def generatePattern(self, length=4):
@@ -100,7 +104,6 @@ class PopChords:
                     chords += [newchord]
                     currentchord = newchord
                     counter += 1
-            print("pattern before replacement: {}".format(chords))
             replaceChord = rn.choice(range(0, 3))
             replaceInterval = rn.choice([2, -2])
             chords[replaceChord] = (chords[replaceChord] + replaceInterval) % 7
@@ -108,6 +111,7 @@ class PopChords:
                 break
             else:
                 chords=[]
+        print("pattern: {}".format(chords))
         return chords
 
     def printtestpattern(self):
@@ -117,27 +121,11 @@ class PopChords:
 
 
 if __name__ == "__main__":
-     blubb = PopChords()
-     mystream = stream.Stream()
-     mystream.insert(0, instrument.Violin())
-     for _ in range(10):
-         beat = blubb.generateGenericBeat(0.125, 2, "pop", "nlubdofn")
-         print(beat)
-         for singlebeat in beat:
-             if singlebeat == 0:
-                 mychord = chord.Chord(["C", "G"], duration=duration.Duration(0.25))
-                 mychord.volume = volume.Volume(velocity=45)
-                 mystream.append((mychord))
+    # {'mood': '3', 'compl': '1', 'tempo': '2', 'parts': 'Chorus, Chorus', 'genre': 'pop', 'key': 'C', 'scale': 'dorian'}
+    #def generate(self, mykey, mycompl, mytempo, myscale, mygenre, singlechordlength=0.25):
 
-                 #mystream.append(note.Rest(duration=duration.Duration(0.25)))
-             else:
-                 mychord2 = chord.Chord(["C", "G"], duration=duration.Duration(0.25 * singlebeat))
-                 mystream.append((mychord2))
-         mystream.append(note.Rest(duration = duration.Duration(8)))
-     #mystream.show('text')
-     mystream.insert(0, tempo.MetronomeMark(number= 60))
-     #mystream.show()
-
-     midi.realtime.StreamPlayer(mystream).play()
-
+    foo = PunkChords()
+    bar = foo.generate('C', 4, 40, "Major", "Punk" )
+    bar.show()
+    midi.realtime.StreamPlayer(bar).play()
 
--- Playground.py
+++ Playground.py
@@ -2,3 +2,16 @@ import json
 from music21 import *
 environment.set("musicxmlPath", r"C:\Program Files\MuseScore 3\bin\MuseScore3.exe")
 
+
+mykey = key.Key("C")
+mykey.show('text')
+mystring = "tinyNotation: 4/4"
+for pitch in mykey.pitches:
+    mystring += " " + str(pitch.name)
+
+print(mystring)
+s = converter.parse("tinyNotation: 4/4 c8 d8 e8")
+s.show('text')
+
+
+s.show()
